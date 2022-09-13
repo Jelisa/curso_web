4c32ffd2a0b45f63130ce334a82e08a295b6ad02
Jelisa <Jelisa@users.noreply.github.com>
2022-09-13 20:50:20 +0200

    19.09.22 - Funcionalidad alarma
    Alarma funcional y dado estilo a la página.

--- modulo_3/relojes/js/script.js
+++ modulo_3/relojes/js/script.js
@@ -1,47 +1,4 @@
-// Add functionality to Reloj1
-
-let intervalo_reloj1
-function startReloj1() {
-    intervalo_reloj1 = setInterval(updateReloj, 1000, "#reloj1");
-}
-function stopReloj1() {
-    clearInterval(intervalo_reloj1);
-    // console.log("click boton stop1")
-    updateReloj("#reloj1");
-}
-
-// Add functionality to Reloj2
-let intervalo_reloj2;
-function functionReloj2(estado) {
-    if (estado == "start") {
-        intervalo_reloj2 = setInterval(updateReloj, 1000, "#reloj2", true)
-    }
-    else if (estado == "stop") {
-        clearInterval(intervalo_reloj2);
-    }
-}
-
-// function updateReloj2() {
-//     let fecha = new Date();
-//     document.querySelector("#reloj2").innerHTML = `${fecha.getHours()}H:${fecha.getMinutes()}M:${fecha.getSeconds()}S`;
-// }
-
-// Añadir funcionalidad 3er boton
-
-const botonInicio3 = document.querySelector("#inicioReloj3");
-const botonDeterner3 = document.querySelector("#stopReloj3");
-let interval_counter;
-
-// botonInicio3.onclick = () => console.log("has hecho click"); //al hacer click cualquier otro evento no funciona
-
-botonInicio3.addEventListener("click", () => {
-                                        interval_counter = setInterval(updateReloj, 1000, "#reloj3")
-                                    });
-botonDeterner3.addEventListener("click", () => {clearInterval});
-
-let intervalo_reloj3;
-
-
+//Función para actualizar los relojes con independencia del método usado.
 
 function updateReloj(selectorId, useSeparator = false) {
     let fecha = new Date();
@@ -53,3 +10,64 @@ function updateReloj(selectorId, useSeparator = false) {
         document.querySelector(selectorId).innerHTML = fecha.toLocaleTimeString();
     }
 }
+
+// Add functionality to Reloj1
+
+let intervalo_reloj1
+function startReloj1() {
+    intervalo_reloj1 = setInterval(updateReloj, 1000, "#reloj1");
+}
+function stopReloj1() {
+    clearInterval(intervalo_reloj1);
+    updateReloj("#reloj1");
+}
+
+// Add functionality to Reloj2
+let intervalo_reloj2;
+function functionReloj2(estado) {
+    if (estado == "start") {
+        intervalo_reloj2 = setInterval(updateReloj, 1000, "#reloj2", "true")
+    }
+    else if (estado == "stop") {
+        clearInterval(intervalo_reloj2);
+    }
+}
+
+// Añadir funcionalidad 3er boton
+
+const botonInicio3 = document.querySelector("#inicioReloj3");
+const botonDeterner3 = document.querySelector("#stopReloj3");
+const botonProgramar = document.querySelector("#programarReloj3");
+
+let interval_counter;
+let alarm_interval;
+console.log(alarm_interval);
+botonInicio3.addEventListener("click", () => {
+                                        interval_counter = setInterval(updateReloj, 1000, "#reloj3",'w')
+                                    });
+botonDeterner3.addEventListener("click", () => {clearInterval(interval_counter),
+                                                updateReloj("#reloj3")});
+
+botonProgramar.addEventListener("click",
+                                () => { if (alarm_interval !== undefined){
+                                            clearInterval(alarm_interval);
+                                        }
+                                        document.getElementById("alarm").innerHTML = "";
+                                        alarm_interval = setInterval(esLaHora, 1000)});
+
+function esLaHora() {
+    let fecha = new Date();
+    console.log('alarm running');
+    if (fecha.getHours() == document.querySelector("#hora").value &&
+        fecha.getMinutes() == document.getElementById("minutos").value &&
+        fecha.getSeconds() == document.getElementById("segundos").value){
+        document.getElementById("alarm").innerHTML = "Es la hora!!"
+        clearInterval(alarm_interval);
+    }
+}
+
+/* Apuntes
+
+// botonInicio3.onclick = () => console.log("has hecho click"); //al hacer click cualquier otro evento no funciona
+
+*/
