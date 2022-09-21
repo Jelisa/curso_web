f47b7e2247dd884d189a4dae09909948171b4bb2
Jelisa <Jelisa@users.noreply.github.com>
2022-09-21 18:16:32 +0200

    21.09.22 -Ejercicio DOM
    Mejora del toogle para mantener el resto de posibles clases que tenga el objeto a parte de la identificativa de visibilidad.
    
    Añadido un boton de toogle del dark mode desde javascript

--- modulo_3/DOM_ejercicio/dom.js
+++ modulo_3/DOM_ejercicio/dom.js
@@ -6,12 +6,19 @@ opcionsMenu.forEach((item, index) => {
 })
 
 const main = document.querySelector('main');
-// console.log(main);
+
+// Añado el botón de modo noche al inicio del documento.
+const buttonDiv = document.createElement("div");
+buttonDiv.innerHTML = "<img  id='night-mode' src='img/dark-mode-toggle-icon.svg' alt=''></img>"
+buttonDiv.style.backgroundColor = "lightgrey";
+buttonDiv.style.width = "20%";
+
+main.insertAdjacentElement("afterbegin", buttonDiv)
 
 main.addEventListener(
     'click',
     (e) => {
-        console.log(e.target.id);
+        // console.log(e.target);
         switch (true) {
             case e.target.classList.value == "preguntas":
                 if (e.target.innerText == "Mostrar respuesta"){
@@ -26,8 +33,8 @@ main.addEventListener(
             case e.target.id == "night-mode":
                 const body = document.querySelector("body");
                 const emText = document.querySelectorAll("em");
-                if (e.target.innerText == "Modo Noche"){
-                    e.target.innerText = "Modo día";
+                if (e.target.getAttribute("src").includes("dark")){
+                    e.target.setAttribute("src", "img/light-mode-toggle-icon.svg")
                     // console.log(body);
                     body.style.backgroundColor = "#2a2c2e";
                     body.style.color = "#f2f2f2";
@@ -41,7 +48,7 @@ main.addEventListener(
                     )
                 }
                 else{
-                    e.target.innerText = "Modo Noche";
+                    e.target.setAttribute("src", "img/dark-mode-toggle-icon.svg");
                     body.style.backgroundColor = "white";
                     body.style.color = "black";
                     emText.forEach(element => element.style.color="black");
@@ -62,14 +69,16 @@ main.addEventListener(
 
 function toogleVisibility(divId){
     let divToShow = document.getElementById(`r-${divId}`)
-    switch (divToShow.getAttribute("class")){
-        case "div-oculto":
-            divToShow.setAttribute("class", "div-shown");
-            break;
-        case "div-shown":
-            divToShow.setAttribute("class", "div-oculto");
-            break;
+    let classes = divToShow.getAttribute("class").split(" ");
+    if (classes.includes("div-oculto")){
+        let idxToModify = classes.indexOf("div-oculto");
+        classes[idxToModify] = "div-shown";
     }
+    else if(classes.includes("div-shown")){
+        let idxToModify = classes.indexOf("div-shown");
+        classes[idxToModify] = "div-oculto";
+    }
+    divToShow.setAttribute("class", classes.join(" "));
 }
 
 // function toogleNightMode(){
