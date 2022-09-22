27cd046816c437a17aeecaa28829148b4a7cae35
Jelisa <Jelisa@users.noreply.github.com>
2022-09-22 17:49:14 +0200

    22.09.22 - Ejercicio DOM 2
    Añado comentarios.
    Arreglo de la función para solucionar el ejercicio 5 del timeout, para llamar correctamente la función.

--- modulo_3/DOM_practica/practica-dom.js
+++ modulo_3/DOM_practica/practica-dom.js
@@ -3,34 +3,38 @@ const footer = document.querySelector("footer");
 
 footer.firstElementChild.innerText = "Jelisa Iglesias"
 
-//Primer ejercicio del 1 al 3
-const lista1 = document.querySelector(".colores-1")
+/* Primer bloque incluye ejercicios del 1 al 3 */
+const lista1 = document.querySelector(".colores-1") // Seleccionar la lista a modificar
 
+// Crear el elemento green to add
 const greenElement = document.createElement("li")
 greenElement.innerText = "green";
 
-lista1.appendChild(greenElement);
+lista1.appendChild(greenElement); // añadir en la última posición el elemento verde
 
-const coralElement = lista1.children[2];
+const coralElement = lista1.children[2]; // seleccionamos el elemento a eliminar
 
+// Crear el elemento brown to add
 const brownElement = document.createElement("li");
 brownElement.innerText = "brown";
 
-lista1.replaceChild(brownElement, coralElement);
+lista1.replaceChild(brownElement, coralElement); // reemplazamos el coral por el brown.
 
+
+
+/* Resto de ejercicios hechos con un único evento general que trata cada uno de los casos */
 
 const main = document.querySelector("main");
 
 main.addEventListener(
     'click',
     (e) => {
-        // console.log(e.target.parentNode.parentNode.id)
         let listaId; // Una variable para guardar los Ids de las listas a actualizar
         switch (e.target.id) {
-            case "button-4":
+            case "button-4": //Solución al  ejercicio 4
+                // Guardo el contador en un atributo idx en el botón para poder elegir el hijo que toca cambiar.
                 listaId = '.colores-2'
                 if (e.target.getAttribute("idx") == null || e.target.getAttribute("idx") == document.querySelector(listaId).children.length) {
-                    // e.target.removeAttribute('idx');
                     e.target.setAttribute('idx', '1');
                     colorList(listaId, 0);
                 }
@@ -40,15 +44,13 @@ main.addEventListener(
                     colorList(listaId, idx)
                 }
                 break;
-            case "button-3":
-                console.log('ejercicio 5')
+            case "button-3": //Solución al ejercicio 5
                 listaId = ".colores-3";
-                console.log(document.querySelector(listaId))
                 for (let i = 0; i < document.querySelector(listaId).children.length; i++) {
-                    setTimeout(colorList(listaId, i), 500)
+                    setTimeout(colorList, 500*i, listaId, i)
                 }
                 break;
-            case "acepto":
+            case "acepto": //Solución al ejercicio 6
                 const buttonToModify = e.target.parentNode.parentNode.children[7]
                 if (e.target.checked) {
                     buttonToModify.removeAttribute("disabled")
@@ -58,8 +60,7 @@ main.addEventListener(
                 }
                 break;
             default:
-                if (e.target.parentNode.parentNode.id == "imagen") {
-                    // console.log('ejercicio 7')
+                if (e.target.parentNode.parentNode.id == "imagen") {//Solución al ejercicio 7
                     const newElement = document.createElement('li');
                     newElement.innerText = e.target.getAttribute("tipo");
                     document.getElementById("frutas").append(newElement);
@@ -69,8 +70,13 @@ main.addEventListener(
 );
 
 function colorList(listSelector, idxToUse) {
+    /* Una función para cambiar el color del texto de uno de los hijos del elemento seleccionado 
+        usando el texto de este como color, y cambiando el color del resto a negro
+        Recibe:
+            **listSelector: un string conteniendo el selector del Elemento padre a seleccionar
+            ** idxToUse: el índice del hijo a cambiar de color.
+    */
     const listToColor = document.querySelector(listSelector).children
-    console.log ('here', listSelector, idxToUse, listToColor)
     listToColor[idxToUse].style.color = listToColor[idxToUse].innerText
     if (idxToUse == 0) {
         listToColor[listToColor.length - 1].style.color = "black";
