a79cefa71b42f60469579f1320e22ff9dbf5fd8a
Jelisa <Jelisa@users.noreply.github.com>
2022-09-28 22:17:19 +0200

    28.9.22 - TO_DO list refactorización y commentarios
    Refactorización del código para:
     - seguir agrupando propiedades según el modelo de vista.
     - utilización de fragmentos a la hora de cambiar el estado de la tarea para evitar la recarga excesiva de la página.
    
    Comento las funciones adecuadamente.

--- modulo_3/DOM_to-do-list/js/app.js
+++ modulo_3/DOM_to-do-list/js/app.js
@@ -1,13 +1,26 @@
-console.log('ok');
-
+// Variables to keep track of the tasks generated.
 let totalTasks = 0;
 let tasks = [];
 
-// Constants containing information derived from the HTML model.
-const formId = "mi-formulario";
-const buttonsParentClass = "opciones";
+// Constants containing information derived from the HTML model and grouped into objects
+// using the type of atribute as a property
+const FORM_INFORMATION = {
+    "formId": "mi-formulario",
+    "inputId" : "tarea_input"
+}
 
+const BUTTONS_PROPERTIES = {
+    "parentClass" : ".opciones",
+    "tagName" : "span"
+}
 
+const TEMPLATE_PROPERTIES = {
+    "templateId" : "task_template",
+    "taskTextClass" : ".tarea_text",
+    "taskContainerClass" : ".tareas"
+}
+
+// States models it stores some of the logic of the states and several properties derived from the HTML
 const STATESPROPERTIES = {
     "pending" : {"buttonsSymbols": ["🔽", "⏬", "❌"],
                 "buttonsTarget" : ["ongoing", "finished", "delete"],
@@ -23,29 +36,43 @@ const STATESPROPERTIES = {
                 "ID": "listaFinalizada"}
 }
 
-const templateToUseId = "task_template";
 
 function createNewTaskFromTemplate(text, state="pending") {
-    const templateClone = document.getElementById(templateToUseId).content.cloneNode(true);
-    const fragment = document.createDocumentFragment();
+    /** A function to create a new task div from the template on the HTML. By default the new task is set to pending. 
+     * @param {String} text contains the text of the task to create
+     * @param {String} state optional parameter used to set the task state. By default is pending.
+     */
 
+    // Clone the template before starting to modify it.
+    const templateClone = document.getElementById(TEMPLATE_PROPERTIES.templateId).content.cloneNode(true);
+    
+    // Set the new task id and text for the cloned template.
     templateClone.id = `task_${totalTasks}`;
-    templateClone.querySelector(".tarea_text").textContent = text;
-
-    let div = templateClone.querySelector(".tareas");
+    templateClone.querySelector(TEMPLATE_PROPERTIES.taskTextClass).textContent = text;
+    
+    // Modify the div properties
+    let div = templateClone.querySelector(TEMPLATE_PROPERTIES.taskContainerClass);
     div.id = `task_${totalTasks}`;
-    div.classList.add(STATESPROPERTIES[state]["class"]);
+    div.classList.add(STATESPROPERTIES[state].class);
     div.setAttribute("task_id", totalTasks);
-
-    let buttons = templateClone.querySelectorAll("span");
+    
+    // Set the buttons
+    let buttons = templateClone.querySelectorAll(BUTTONS_PROPERTIES.tagName);
     modifyButtons(buttons, STATESPROPERTIES[state].buttonsSymbols, STATESPROPERTIES[state].buttonsTarget);
-
-    let opciones = templateClone.querySelector(`.${buttonsParentClass}`);
+    
+    // Set the state of the task using the buttons containers.
+    let opciones = templateClone.querySelector(BUTTONS_PROPERTIES.parentClass);
     opciones.setAttribute("task_id", totalTasks);
     opciones.setAttribute("state", state);
-
+    
+    // Create a new fragment and append the modified template to it
+    const fragment = document.createDocumentFragment();
     fragment.appendChild(templateClone);
+
+    // Place the fragment inside the pending tasks list at the last position.
     document.getElementById(STATESPROPERTIES[state].ID).appendChild(fragment);
+
+    // Add the task_id to the list of tasks ever created.
     tasks.push(`task_${totalTasks}`);
 }
 
@@ -55,8 +82,8 @@ main.addEventListener(
     "click",
     (e) => {
         let eventParentNode = e.target.parentNode;
-        if (e.target.tagName == "BUTTON" && eventParentNode.id == formId) {
-            const formTextField = document.getElementById("tarea");
+        if (e.target.tagName == "BUTTON" && eventParentNode.id == FORM_INFORMATION.formId) {
+            const formTextField = document.getElementById(FORM_INFORMATION.inputId);
             if (formTextField.value != "") {
                 createNewTaskFromTemplate(formTextField.value);
                 totalTasks++;
@@ -84,22 +111,46 @@ main.addEventListener(
 
 
 
-function modifyTask(task, currentState, newState) {
-    /**
-     * 
+function modifyTask(taskId, currentState, newState) {
+    /** A function to modify a Node of the DOM identified by the parameter task. 
+     * In order to do the modifications needed without causing reflow of the page the node is temporary stored in a 
+        DocumentFragment variable.
+     * @param {String} taskId: contains the id of the element to modify
+     * @param {String} currentState: contains the name of the element actual state.
+     * @param {String} newState: contains the name of the state to apply.
      */
-    const buttonsContainer = document.querySelector(`#${task}>.${buttonsParentClass}`)
-    let buttons = document.querySelectorAll(`#${task}>.${buttonsParentClass}>span`);
-    modifyButtons(buttons, STATESPROPERTIES[newState].buttonsSymbols, STATESPROPERTIES[newState].buttonsTarget);
-    moveFromTo(document.getElementById(task), currentState, newState)
-    buttonsContainer.setAttribute("state", newState)
+
+    const TEMPORARY_FRAGMENT = document.createDocumentFragment();
+    const ELEMENT_TO_MODIFY = document.getElementById(taskId);
+
+    /* The following instruction removes the element from the DOM, 
+    this way we can modify it without causing reflow of the page */
+    TEMPORARY_FRAGMENT.appendChild(ELEMENT_TO_MODIFY);
+
+    // Modify the buttons container where the state of the element is stored to the new state to apply.
+    const BUTTONS_CONTAINER = TEMPORARY_FRAGMENT.querySelector(BUTTONS_PROPERTIES.parentClass)
+    BUTTONS_CONTAINER.setAttribute("state", newState)
+
+    // Replace the current buttons with the buttons from the new state 
+    const BUTTONS = TEMPORARY_FRAGMENT.querySelectorAll(BUTTONS_PROPERTIES.tagName);
+    modifyButtons(BUTTONS, STATESPROPERTIES[newState].buttonsSymbols, STATESPROPERTIES[newState].buttonsTarget);
+
+    // Change the class responsible of the format associated to the state 
+    toogleClass(TEMPORARY_FRAGMENT.firstElementChild, STATESPROPERTIES[currentState].class, STATESPROPERTIES[newState].class)
+
+    // Move the fragment back to the DOM in the area for the new state.
+    moveTo(TEMPORARY_FRAGMENT, newState);
 }
 
-function modifyButtons(oldButtonsContainer, newButtons, newStates) {
-    /**
-     * 
+function modifyButtons(oldButtonsItterable, newButtons, newStates) {
+    /**A function to iterate over the elements given by the first parameter using a forEach loop in order to modify 
+     * the content and targetState attribute of the aforementioned elements.
+     * @param {Iterable} oldButtonsItterable: a itterable object with the method forEach
+     *                           will be looped through in order to modify the elements for the new ones
+     * @param {Array} newButtons: an array like object accessible through indexes containing the new information to use.
+     * @param {String} newStates: a string containing the new state to apply to the targetState attribute of each new button.
      */
-    oldButtonsContainer.forEach(
+    oldButtonsItterable.forEach(
         (button, idx) => {
             button.textContent = newButtons[idx]
             button.setAttribute("targetState", newStates[idx]);
@@ -108,20 +159,33 @@ function modifyButtons(oldButtonsContainer, newButtons, newStates) {
     )
 }
 
-function moveFromTo(element, currentState, newState){
-    /**
-     * 
+function moveTo(element, newState){
+    /** A function to change the parent of a Node. 
+     * It receives a Node (or Node-like) object and a string, and changes the node parent by using the STATESPROPERTIES
+     * constant in combination with the new state provided (used as key) to select the new parent by Id and then append 
+     * the node.
+     * @param {Node} element: a Node or Node-like object to move to a new parent.
+     * @param {String} newState: a string containing the new state to move to.
      */
     const newParent = document.getElementById(STATESPROPERTIES[newState].ID);
-    toogleClass(element, STATESPROPERTIES[currentState]["class"], STATESPROPERTIES[newState]["class"])
     newParent.appendChild(element);
 }
 
 function toogleClass(element, oldClassName, newClassName) {
-    /**
-     * 
+    /** It should receive a Node (or Node-like) object with the oldClassName string inside the class attribute, which
+     *  will be replaced by the newClassName string while maintaining all the other classes of the object.
+     * Params:
+     * @param {Node} element: a Node or Node-like object to which the classes should be modified.
+     * @param {String} oldClassName: a string containing the old class name to modify.
+     * @param {String} newState: a string containing the new class name to apply.
      */
+    
+    // Convert the classes attribute to a list
     let classesArray = element.classList.value.split(" ");
+
+    // Modify the array changin the desired class for the new one
     classesArray.splice(classesArray.indexOf(oldClassName), 1, newClassName)
+
+    // Set the element class attribute to the new string containing all the classes.
     element.setAttribute('class', classesArray.join(" "))
 }
