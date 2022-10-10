e6d9ddaf68d8ed17b918690af87c82527a0d89d7
Jelisa <Jelisa@users.noreply.github.com>
2022-10-10 19:03:09 +0200

    10.10.22 - Uso de APIs
    Creación del contenido desde la web.
    Implementacion de funcionalidad de búsqueda.

--- modulo_2/apis/api_breaking_bad/js/class.api.js
+++ modulo_2/apis/api_breaking_bad/js/class.api.js
@@ -3,6 +3,10 @@ export default class ApiRest {
         this.rootURL = rootURL; // Setting the root for the API
     }
     async get (endPoint){
+        /**A function to obtain the json information from a given endpoint
+         * @param {string} endPoint: a string containing the endpoint to look for.
+         */
+
         const URL = this.rootURL + endPoint; // Generating the full URL
         
         const PETICION = await fetch(URL, {
@@ -17,5 +21,27 @@ export default class ApiRest {
         else{
             return PETICION.statusText;
         }
+    }
+
+    // Qué queremos mostrar y donde.
+    pintarPersonajes(listaPersonajes, objectoDOM){
+        /**
+         * @param {Array} listaPersonajes - An array containing the object from the Json.
+         */
+
+        objectoDOM.innerHTML = "";
+        for (let personaje of listaPersonajes){
+            objectoDOM.innerHTML += `
+                <div class="col col-sm-4">
+                    <div class="card">
+                        <img src="${personaje.img}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <p class="card-title h5">${personaje.name}</p>
+                        <p class="card-text">${personaje.occupation}</p>
+                        <p class="card-text">${personaje.status}</p>
+                        </div>
+                    </div>
+                </div>`;
+        };
     }
 }
--- modulo_2/apis/api_breaking_bad/js/main.js
+++ modulo_2/apis/api_breaking_bad/js/main.js
@@ -2,12 +2,15 @@ import ApiRest from "./class.api.js"; // import the module to operate the api
 
 const BTN_BUSCAR = document.getElementById("btnBuscar"); // El boton de búsqueda de personaje
 const INPUT_PERSONAJE = document.getElementById("searchInput"); // The input of the character search button.
+const CHARACTER_CONTAINER = document.getElementById("charactersContainer");
 
 // Breaking Bad API
 const BREAKING_BAD_API_ROOT_URL = "https://breakingbadapi.com/api/";
 const BREAKIN_BAD_OBJECT = new ApiRest(BREAKING_BAD_API_ROOT_URL);
-BREAKIN_BAD_OBJECT.get("characters/1")
-// console.log(INPUT_PERSONAJE.value);
+BREAKIN_BAD_OBJECT.get("characters")
+    .then(result => {
+        BREAKIN_BAD_OBJECT.pintarPersonajes(result, CHARACTER_CONTAINER);
+    });
 
 BTN_BUSCAR.addEventListener("click", obtener_Personaje)
 
@@ -15,8 +18,16 @@ function obtener_Personaje (e){
     /**
      * @param {Event} e: the event that triggers the function
      */
+
     // Prevent the default functionality of the button
     e.preventDefault();
-    let characterToLookFor = INPUT_PERSONAJE.value;
-
+    // Retrive the name to search
+    let characterToLookFor = INPUT_PERSONAJE.value; 
+    // Following the API documentation we can look for a name using the following string
+    let endPoint = "characters?name=" + characterToLookFor.replace(" ","+") 
+    BREAKIN_BAD_OBJECT.get( endPoint)
+        .then(result => {
+            BREAKIN_BAD_OBJECT.pintarPersonajes(result, CHARACTER_CONTAINER);
+        });
 }
+
