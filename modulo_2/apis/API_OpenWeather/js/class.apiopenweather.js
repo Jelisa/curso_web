b786b48ca5dd6a4975af43228bebff8e7e56ef86
Jelisa <Jelisa@users.noreply.github.com>
2022-10-11 20:34:11 +0200

    11.10.22 - OpenWeather app de la API
    Set up de la App para que muestre el tiempo actual en una ciudad elegida en el formulario usando la API de OpenWeatherMap.
    Empieza implementación del forecast.

---
+++ modulo_2/apis/API_OpenWeather/js/class.apiopenweather.js
@@ -0,0 +1,48 @@
+export default class ApiRest {
+    constructor(rootURL) {
+        this.rootURL = rootURL; // Setting the root for the API
+    }
+    async get (endPoint){
+        /**A function to obtain the json information from a given endpoint
+         * @param {string} endPoint: a string containing the endpoint to look for.
+         */
+
+        const URL = this.rootURL + endPoint; // Generating the full URL
+        
+        const PETICION = await fetch(URL, {
+            method:"GET", // Select on the Server
+            headers: { "Accept": "application/json"} // Specify it's a json object
+        })
+
+        if(PETICION.status === 200){
+            let json = await PETICION.json()
+            return json;
+        }
+        else{
+            return PETICION.statusText;
+        }
+    }
+
+    // Qué queremos mostrar y donde.
+    Current_Weather(citisCurrentWeather, objectoDOM){
+        /**
+         * @param {Array} listaPersonajes - The Json with the city's weather to show.
+         * @param {Node} objectoDOM - The Node to modify with the info from the API
+         */
+
+        const CONTENT_ORIGINAL_PARENT = objectoDOM.parentNode;
+        
+        const NEW_FRAGMENT = document.createDocumentFragment();
+        NEW_FRAGMENT.appendChild(objectoDOM);
+        NEW_FRAGMENT.getElementById("cityAndCountry").textContent = 
+            `${citisCurrentWeather.name.toUpperCase()} / ${citisCurrentWeather.sys.country.toUpperCase()}`
+        NEW_FRAGMENT.getElementById("skyes").innerHTML = 
+            `${citisCurrentWeather.weather[0].description.toUpperCase()} 
+            <img src="http://openweathermap.org/img/wn/${citisCurrentWeather.weather[0].icon}@2x.png" alt="">`
+        NEW_FRAGMENT.getElementById("temperature").textContent = `${citisCurrentWeather.main.temp} º C`
+        NEW_FRAGMENT.getElementById("humidity").textContent = `Humidity: ${citisCurrentWeather.main.humidity} %`
+        NEW_FRAGMENT.getElementById("wind").textContent = `Wind: ${citisCurrentWeather.wind.speed} m/s`
+        CONTENT_ORIGINAL_PARENT.appendChild(NEW_FRAGMENT)
+    }
+    
+}
---
+++ modulo_2/apis/API_OpenWeather/js/main.js
@@ -0,0 +1,75 @@
+import ApiRest from "./class.apiopenweather.js";
+
+// Get the buttons that will send and get information from the API
+const BTN_GET_DATA = document.getElementById("changeCity");
+const BTN_GET_FORECAST = document.getElementById("forecast");
+
+// Set the parameters needed for the API
+const OPENWEATHER_API_ROOT = "http://api.openweathermap.org/";
+const OPENWEATHER_API_OBJECT = new ApiRest(OPENWEATHER_API_ROOT);
+const API_KEY = "APPID=6c9744b35586f6b311223250a34696cf";
+
+// Get the content to modify with the API Information
+const CURRENT_WEATHER_CARD = document.getElementById("currentWeather");
+const FORECAST_CONTAINER = document.getElementById("forecast");
+
+// let endPoint = `data/2.5/weather?q=${DEFAULT_CITY},${DEFAULT_COUNTRY}&${API_KEY}&units=metric`
+
+getDataFromApi();
+
+BTN_GET_DATA.addEventListener(
+    'click',
+    (e) => {
+        e.preventDefault();
+        getDataFromApi();
+    }
+)
+
+function getDataFromApi(){
+    let country = document.getElementById("formCountry").value;
+    let city = document.getElementById("formCity").value;
+    let currentWeatherEndPoint = `data/2.5/weather?q=${city},${country}&${API_KEY}&units=metric`
+    OPENWEATHER_API_OBJECT.get(currentWeatherEndPoint)
+        .then(result => {
+            if (result === "Not Found" || result === "Bad Request"){
+                console.log("TCL: getDataFromApi -> result", result)
+                if (!checkErrorMessage()){
+                    printError();}
+            }
+            else{
+                if (checkErrorMessage()){
+                    deleteErrorMessage();
+                }
+                OPENWEATHER_API_OBJECT.Current_Weather(result, CURRENT_WEATHER_CARD);
+				console.log("TCL: getDataFromApi -> result", result.coord.lon);
+                
+                let forecastEndPoint = `data/2.5/forecast?lat=${result.coord.lat}&lon=${result.coord.lon}&${API_KEY}&units=metric`;
+                console.log(FORECAST_CONTAINER, forecastEndPoint)
+            }
+        });
+}
+
+function printError(){
+    const ERROR_NODE = document.createElement('p');
+    ERROR_NODE.textContent = "Invalid City and/or Country"
+    ERROR_NODE.id = "errorMessage";
+    ERROR_NODE.classList.add("text-danger")
+    const CITY_LINE = document.getElementById("cityAndCountry");
+    CITY_LINE.insertAdjacentElement("beforebegin", ERROR_NODE);
+
+}
+
+function checkErrorMessage(){
+    const ERROR_NODE = document.getElementById("errorMessage");
+    if (ERROR_NODE == null){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function deleteErrorMessage(){
+    const ERROR_NODE = document.getElementById("errorMessage");
+    ERROR_NODE.remove();
+}
