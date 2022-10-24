e5c8876f60bc05215aed90fbb15fbd0f643adfa5
Jelisa <Jelisa@users.noreply.github.com>
2022-10-24 19:04:24 +0200

    24.10.22 - Teoria

---
+++ modulo_2/NODE/modulos/path.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+console.log(path)
+
+const ruta = "/home/jelisa/Documents/GitHub/curso_web/modulo_2/NODE/modulos/path.js"
+console.log(path.parse(ruta))
+
+console.log(path.resolve('aqui/windows'))
