ea8238f35a97a663c0238f2b9a9f896fca29d07d
Jelisa <Jelisa@users.noreply.github.com>
2022-11-07 20:32:10 +0100

    7.11.22 - API de chat basica

---
+++ modulo_2/aplicaciones_distribuidas/chat/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const APP = express();
+
+const HTTP = require('http').Server(APP);
+const io = require('socket.io')(HTTP);
+
+const PORT = process.env.PORT || 0;
+
+console.log(PORT);
+APP.get('/', (req, res) => {
+    res.render("index.ejs")
+})
+
+io.sockets.on("connection", (socket) => {
+    console.log('hola');
+    /** Socket listening for the variable username */
+    socket.on('username', (username) => {
+        console.log(username);
+        socket.username = username;
+        io.emit("is_online", `<i> ${socket.username} se ha unido al chat.</i>`)
+    })
+
+    socket.on("disconnect", (username)=>{
+        socket.username = username;
+        io.emit("is_online", `<i> ${socket.username} se ha desconectado del chat.</i>`)
+    })
+
+    socket.on("chat_message", (message) => {
+        io.emit("is_online", `<strong> ${socket.username}.</strong> ${message}`)
+    })
+
+})
+
+const server = HTTP.listen (PORT, () => {
+    console.log(`Escuchando por el puerto ${server.address().port}`);
+})
